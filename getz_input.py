c02c392c376287f81b6bac89cd3044b230c4bd2d
georgezyvg <georgeputta355@gmail.com>
2025-05-22 14:10:07 +0100

    Update getz_input.py

--- getz_input.py
+++ getz_input.py
@@ -103,7 +103,7 @@ def getSignableTxn(parsed):
 #==============================================================================
 def HASH160(pubk_hex):
     iscompressed = True if len(pubk_hex) < 70 else False
-    P = ice.pub2pub(pubk_hex)
+    P = pub2upub(pubk_hex)
     return ice.pubkey_to_h160(0, iscompressed, P).hex()
 #==============================================================================
 
